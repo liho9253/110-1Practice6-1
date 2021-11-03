5f3eed62f361d0bf9ba7e2f316c7382628cb968f
偉國 曾 <108111113@mail.aeust.edu.tw>
2021-11-03 14:51:48 +0800

    CheckBox

--- 110-1Practice6-1/CheckBox.aspx.cs
+++ 110-1Practice6-1/CheckBox.aspx.cs
@@ -10,5 +10,14 @@ namespace _110_1Practice6_1 {
         protected void Page_Load(object sender, EventArgs e) {
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e){
+            String temp = "";
+            for (int i = 0; i < CheckBoxList1.Items.Count;i++) {
+                if(CheckBoxList1.Items[i].Selected)
+                    temp += CheckBoxList1.Items[i].Value + "\n";
+            }
+            TextBox1.Text = temp;
+        }
     }
 }
